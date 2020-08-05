24f1d66619db23a5bf7d1ce1638ee6ae48134ef9
Yichen Li <liyichen20000104@outlook.com>
2020-08-05 12:21:37 -0400

    WIP: Card and Deck ADTs

--- src/csci4963u20/project/doudizhu/Application.java
+++ src/csci4963u20/project/doudizhu/Application.java
@@ -4,5 +4,20 @@ public class Application {
 
     public static void main(String[] args){
         System.out.println("Welcome to Doudizhu!");
+        Deck d = new Deck(10);
+        d.add(new Card(14, "A"));
+        d.add(new Card(15, "2"));
+        d.add(new Card(16, "Joker"));
+        d.add(new Card(4, "4"));
+        d.add(new Card(5, "5"));
+        d.add(new Card(11, "J"));
+        d.add(new Card(12, "Q"));
+        d.add(new Card(13, "K"));
+        d.add(new Card(13, "K"));
+        d.add(new Card(11, "J"));
+
+        d.sortDeck();
+
+        d.printDeck();
     }
 }
--- src/csci4963u20/project/doudizhu/Deck.java
+++ src/csci4963u20/project/doudizhu/Deck.java
@@ -5,13 +5,55 @@ public class Deck {
     private ArrayList<Card> cards;
     private int card_count;
 
+    /**
+     * Default empty constructor.
+     */
     public Deck(){
         cards = new ArrayList<>();
         card_count = 0;
     }
 
+    /**
+     * Constructor from deck size.
+     * @param size deck size
+     */
     public Deck(int size){
         card_count = size;
         cards = new ArrayList<>(card_count);
+    }
+
+    /**
+     * Add a Card to this Deck.
+     * @param c Card to be added
+     * @return boolean if Card is added successfully
+     */
+    public boolean add(Card c){
+        boolean ret = cards.add(c);
+        if(ret){ card_count++; }
+        return ret;
+    }
+
+
+    /**
+     * Shuffle the deck.
+     */
+    public void shuffle(){
+        Collections.shuffle(cards);
+    }
+
+    /**
+     * Sort the deck.
+     */
+    public void sortDeck(){
+        Collections.sort(cards);
+    }
+
+    /**
+     * Print the deck.
+     */
+    public void printDeck(){
+        for(Card c:cards){
+            System.out.println(c.getName());
+        }
     }
 }
