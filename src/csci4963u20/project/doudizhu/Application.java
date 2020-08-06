7109d15bad8125a7dc02da168085df8f8c5fce9f
chrroger <58056857+chrroger@users.noreply.github.com>
2020-08-06 13:08:22 -0400

    Update Application.java

--- src/csci4963u20/project/doudizhu/Application.java
+++ src/csci4963u20/project/doudizhu/Application.java
@@ -35,12 +35,62 @@ public class Application {
         trio_single.add(new Card(11, "J"));
         System.out.println(trio_single.isTrioSingle());
         
+        // (发牌) deal card
         ArrayList<String> boxes = new ArrayList<>();
         ArrayList<String> numbers = new ArrayList<>();
         ArrayList<String> names = new ArrayList<>();
-        for(int i = 1; i <= 16; i++)
+        for(int i = 3; i <= 16; i++)
         {
             numbers.add(i+"");
+        }
+        numbers.add(16+"");
+        for(int i = 3; i <= 10; i++)
+        {
+            names.add(i+"");
+        }
+        names.add("J");
+        names.add("Q");
+        names.add("K");
+        names.add("A");
+        names.add("2");
+        names.add("joker");
+        names.add("joker");
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j <= 12; j++)
+            {
+                boxes.add(names[i] + numbers[i]);
+            }
+        }
+        boxes.add(names[13] + numbers[13]);
+        boxes.add(names[14] + numbers[14]);
+        Collections.shuffle(boxes);
+        ArrayList<String> player1 = new ArrayList<>();
+        ArrayList<String> player2 = new ArrayList<>();
+        ArrayList<String> player3 = new ArrayList<>();
+        ArrayList<String> dipai = new ArrayList<>();
+        
+        for(int i = 0; i < boxes.size(); i++)
+        {
+            if(i > 50)
+            {
+                dipai.add(boxes.get(i));
+            }
+            else
+            {
+                if (i % 3 == 0)
+                {
+                    player1.add(boxes.get(i));
+                }
+                else if(i % 3 == 1)
+                {
+                    player2.add(boxes.get(i));
+                }
+                else
+                {
+                    player3.add(boxes.get(i));
+                }
+            }
         }
     }
 }
