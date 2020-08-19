67b4f97e7e725e91e68a91234b43c0e91a6486c6
Ivan <sunq3@rpi.edu>
2020-08-19 15:22:22 -0400

    Merge branch 'master' of https://github.com/yichen0104/java_project
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/csci4963u20/project/doudizhu/OpponentStatusPanel.java
+++ src/csci4963u20/project/doudizhu/OpponentStatusPanel.java
@@ -6,23 +6,40 @@ import java.awt.*;
 public class OpponentStatusPanel extends JPanel {
     public String playerName;
     public JTextField cards_remaining;
+    JButton status_button;
 
-    public OpponentStatusPanel(){
+    public OpponentStatusPanel(String Name){
         cards_remaining = new JTextField();
+        cards_remaining.setEditable(false);
         // *******TEST********
-        playerName = "TEST";
+        playerName = Name;
         // *******************
         setBorder(BorderFactory.createTitledBorder(String.format("Player %s", playerName)));
-
+        status_button=new JButton();
+        status_button.setBackground(Color.blue);
+        status_button.setOpaque(true);
         setLayout(new GridLayout(1, 2));
         add(new JLabel("Cards remaining: "));
+        add(status_button);
         cards_remaining.setText("17");
         Font font1 = new Font("SansSerif", Font.BOLD, 30);
         cards_remaining.setFont(font1);
         cards_remaining.setHorizontalAlignment(JTextField.CENTER);
 
-
         add(cards_remaining);
     }
+    public void updaterRemain(String a){
+        cards_remaining.setText(a);
+    }
+    public void statusActivate(){
+        status_button.setBackground(Color.red);
+        status_button.setOpaque(true);
+    }
+
+    public void statusDeactivate(){
+        status_button.setBackground(Color.blue);
+        status_button.setOpaque(true);
+    }
+
 
 }
